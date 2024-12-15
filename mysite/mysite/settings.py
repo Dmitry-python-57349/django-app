e99f4ebb52c67e833391633c3b20fd7de0642a49
Dmitry <dimas2568.shipilo@mail.ru>
2024-12-15 07:30:28 +0300

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hvxn%qq=gyw^4*o2lo1#bw0=wh#ux9s8h!=@c608arf_gz3+^7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["176.124.214.97"]
 
 
 # Application definition
